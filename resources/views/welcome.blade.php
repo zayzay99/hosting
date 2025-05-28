0b762121e2aa1ef9e14d3dc66ae08ddded2d176a
zayzay99 <zayyidanalkhausar@gmail.com>
2025-05-28 09:23:27 +0700

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    <center>
+    
           <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -25,7 +25,7 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-    </center>
+    
   
 </body>
 </html>
