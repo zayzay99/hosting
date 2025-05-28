d877b951c7e9aa6fe7302247d43e44691d9d77c9
zayzay99 <zayyidanalkhausar@gmail.com>
2025-05-28 09:21:28 +0700

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
+    <center>
+          <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -24,5 +25,7 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+    </center>
+  
 </body>
 </html>
